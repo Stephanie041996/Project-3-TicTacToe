954eb0c5bc0651fbd2c3e7367137781c934b4372
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-04-01 10:06:42 +0200

    Update validate method

--- bin/board.rb
+++ bin/board.rb
@@ -1,16 +1,38 @@
-def validate
-  move = []
-  input = 0
-  3.times do
-    puts 'enter number'
-    input = gets.chomp
-    if move.include?(input)
-      puts 'already there'
-    else
-      move << input
-    end
+# def validate
+#   move = []
+#   input = 0
+#   3.times do
+#     puts 'enter number'
+#     input = gets.chomp
+#     if move.include?(input)
+#       puts 'already there'
+#     else
+#       move << input
+#     end
+#   end
+#   move
+# end
+
+# puts validate
+
+puts 'player 1 its your turn '
+
+def play(input)
+  moves = [10]
+
+  if moves.include?(input)
+    puts ' try again'
+    false
+  else
+
+    moves << input
+    true
   end
-  move
 end
 
-puts validate
+turn = false
+while turn == false
+  input = gets.chomp.to_i
+  turn = play(input)
+
+end
