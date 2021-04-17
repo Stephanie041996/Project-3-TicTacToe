6cd1082bce889fd39980e058d1c7bed189c3231b
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-04-17 19:37:01 +0200

    add test for userinterface class, initialize method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,2 +1,13 @@
 require_relative '../lib/board'
 require_relative '../lib/game'
+
+describe UserInterface do
+  let(:userinterface) { described_class.new }
+
+  describe '#initialize' do
+    it 'raises arg error if arg passed ' do
+      expect { userinterface('str') }.to raise_error(ArgumentError)
+    end
+  end
+
+end
