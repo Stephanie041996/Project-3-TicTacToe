f0cd54b01f4b0177b42d5ca91bca12b98273f1f3
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-04-17 22:58:53 +0200

    made required changes

---
+++ spec/board_spec.rb
@@ -0,0 +1,42 @@
+
+require_relative '../lib/board'
+
+describe Board do
+  subject(:board) { described_class.new }
+
+  describe '#initialize' do
+    it 'raises arg error if arg passed ' do
+      expect { board('str') }.to raise_error(ArgumentError)
+    end
+
+    it 'returns an array with 9 empty spaces' do
+      expect(subject.board).to be_an(Array)
+    end
+
+    it 'contains a win sequence thats an array' do
+      expect(subject.win_sequence).to be_an(Array)
+    end
+  end
+
+  describe 'tie?' do
+    context 'returning tie' do
+      it 'returns true if all are string' do
+        expect(board.tie?(%w[x x x x o o o o o])).to be true
+      end
+      it 'returns false if all are not string' do
+        expect(board.tie?(['x', 'x', 'x', 'x', 'o', 'o', 'o', nil, nil])).to be false
+      end
+    end
+  end
+
+  describe 'Win?' do
+    context 'when win combination match' do
+      it 'returns true if there are any winning combo' do
+        expect(board.win?(['x', 'x', 'x', 4, 5, 6, 7, 8, 9])).to eq true
+      end
+      it 'returns false if  there are no winning combo' do
+        expect(board.win?([1, 2, 3, 4, 5, 6, 7, 8, 9])).to eq false
+      end
+    end
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/board'
+
 require_relative '../lib/game'
 
 describe UserInterface do
@@ -43,46 +43,6 @@ describe UserInterface do
     context 'when input is > 9'
     it 'returns false if player input is greater than 9' do
       expect(userinterface.num_check2(11)).to eq(false)
-    end
-  end
-end
-
-describe Board do
-  subject(:board) { described_class.new }
-
-  describe '#initialize' do
-    it 'raises arg error if arg passed ' do
-      expect { board('str') }.to raise_error(ArgumentError)
-    end
-
-    it 'returns an array with 9 empty spaces' do
-      expect(subject.board).to be_an(Array)
-    end
-
-    it 'contains a win sequence thats an array' do
-      expect(subject.win_sequence).to be_an(Array)
-    end
-  end
-
-  describe 'tie?' do
-    context 'returning tie' do
-      it 'returns true if all are string' do
-        expect(board.tie?(%w[x x x x o o o o o])).to be true
-      end
-      it 'returns false if all are not string' do
-        expect(board.tie?(['x', 'x', 'x', 'x', 'o', 'o', 'o', nil, nil])).to be false
-      end
-    end
-  end
-
-  describe 'Win?' do
-    context 'when win combination match' do
-      it 'returns true if there are any winning combo' do
-        expect(board.win?(['x', 'x', 'x', 4, 5, 6, 7, 8, 9])).to eq true
-      end
-      it 'returns false if  there are no winning combo' do
-        expect(board.win?([1, 2, 3, 4, 5, 6, 7, 8, 9])).to eq false
-      end
     end
   end
 end
